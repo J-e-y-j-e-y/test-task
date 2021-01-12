34f464eb0fad04fe6b04073f5e9fe5d5dfc9a60e
July <Nightmare-000@yandex.ru>
2021-01-12 22:12:51 +0400

    Client class added

--- src/main/java/com/haulmont/testtask/MainUI.java
+++ src/main/java/com/haulmont/testtask/MainUI.java
@@ -3,6 +3,9 @@ package com.haulmont.testtask;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.*;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.themes.ValoTheme;
 
 @Theme(ValoTheme.THEME_NAME)
@@ -10,12 +13,35 @@ public class MainUI extends UI {
 
     @Override
     protected void init(VaadinRequest request) {
-        VerticalLayout layout = new VerticalLayout();
+        VerticalLayout todosList = new VerticalLayout(); // (1)
+        todosList.setSizeFull();
+        todosList.setMargin(true);
+        TextField taskField = new TextField(); // (2)
+        Button addButton = new Button("Add"); // (3)
+        todosList.addComponent(new Label("Vaadin Todo"));
+        todosList.addComponent(addButton);
+        todosList.addComponent(taskField);
+        addButton.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent clickEvent) {
+                Label checkbox = new Label(taskField.getValue());
+                todosList.addComponent(checkbox);
+            }
+        });
+        //addButton.addClickListener(click -> {
+            // (4)
+            //Checkbox checkbox = new Checkbox(taskField.getValue());
+            //todosList.add(checkbox);
+        //});
+
+
+
+        /*VerticalLayout layout = new VerticalLayout();
         layout.setSizeFull();
         layout.setMargin(true);
 
         layout.addComponent(new Label("Main UI"));
-
-        setContent(layout);
+*/
+        setContent(todosList);
     }
 }
