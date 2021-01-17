67893593a3282359ab15043211d11cb2e52e2837
July <Nightmare-000@yandex.ru>
2021-01-18 02:11:52 +0400

    DAO UserController

---
+++ src/main/java/com/haulmont/testtask/AbstractController.java
@@ -0,0 +1,50 @@
+package com.haulmont.testtask;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public abstract class AbstractController <E, K>{
+    private Connection connection;
+
+    public AbstractController() {
+        try {
+            connection = DriverManager.getConnection("jdbc:hsqldb:file:/../db-data/testdb", "SA", "");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public abstract List<E> getAll();
+    public abstract E update(E entity);
+    public abstract E getEntityById(K id);
+    public abstract boolean delete(K id);
+    public abstract boolean create(E entity);
+
+
+    // Получение экземпляра PrepareStatement
+    public PreparedStatement getPrepareStatement(String sql) {
+        PreparedStatement ps = null;
+        try {
+            ps = connection.prepareStatement(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return ps;
+    }
+
+    // Закрытие PrepareStatement
+    public void closePrepareStatement(PreparedStatement ps) {
+        if (ps != null) {
+            try {
+                ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
--- src/main/java/com/haulmont/testtask/MainUI.java
+++ src/main/java/com/haulmont/testtask/MainUI.java
@@ -1,6 +1,7 @@
 package com.haulmont.testtask;
 
 import com.vaadin.annotations.Theme;
+import com.vaadin.event.SelectionEvent;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.*;
 import com.vaadin.ui.Button;
@@ -13,7 +14,7 @@ public class MainUI extends UI {
 
     @Override
     protected void init(VaadinRequest request) {
-        VerticalLayout todosList = new VerticalLayout(); // (1)
+        /*VerticalLayout todosList = new VerticalLayout(); // (1)
         todosList.setSizeFull();
         todosList.setMargin(true);
         TextField taskField = new TextField(); // (2)
@@ -28,6 +29,7 @@ public class MainUI extends UI {
                 todosList.addComponent(checkbox);
             }
         });
+        */
         //addButton.addClickListener(click -> {
             // (4)
             //Checkbox checkbox = new Checkbox(taskField.getValue());
@@ -36,12 +38,45 @@ public class MainUI extends UI {
 
 
 
-        /*VerticalLayout layout = new VerticalLayout();
-        layout.setSizeFull();
-        layout.setMargin(true);
+        VerticalLayout vertLayout = new VerticalLayout();
+        HorizontalLayout horizLayout = new HorizontalLayout();
+        vertLayout.setSizeFull();
+        vertLayout.setMargin(true);
 
-        layout.addComponent(new Label("Main UI"));
-*/
-        setContent(todosList);
+
+        Grid gridTable = new Grid();
+        Grid buttonsGrid = new Grid();
+
+        buttonsGrid.addColumn("Options");
+        buttonsGrid.addRow("Clients");
+        buttonsGrid.addRow("Credits");
+        buttonsGrid.addRow("Banks");
+        gridTable.addSelectionListener(new SelectionEvent.SelectionListener() {
+            @Override
+            public void select(SelectionEvent selectionEvent) {
+                String rowName = selectionEvent.getSelected().toString();
+                System.out.println(rowName);
+                if(rowName.equals("Clients")){
+                    gridTable.addColumn("Name");
+                    gridTable.addColumn("Phone");
+                    gridTable.addColumn("Email");
+                    gridTable.addColumn("Passport");
+                }else if(rowName.equals("Credits")){
+                    gridTable.addColumn("Limit");
+                    gridTable.addColumn("Procent");
+                }else if(rowName.equals("Banks")){
+
+                }
+            }
+        });
+
+        horizLayout.setWidthUndefined(); // Default, can be omitted
+        horizLayout.setSpacing(false); // Compact layout
+
+        horizLayout.addComponent(buttonsGrid);
+        horizLayout.addComponent(gridTable);
+        setContent(horizLayout);
+
     }
+
 }
